8892954cefb5754b0d98c33f1c2aa353a30ec895
tolik1234-ai <anatoliygusenkov2008@gmail.com>
2026-06-26 22:17:21 +0300

    ERC1155Airdroper added

---
+++ contracts/ERC1155Airdroper/ERC1155Airdroper.sol
@@ -0,0 +1,54 @@
+// SPDX-License-Identifier: MIT
+pragma solidity^0.8.29;
+
+import "../IUtilityContract.sol";
+import "@openzeppelin/contracts/token/ERC1155/IERC1155.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract ERC1155Airdroper is IUtilityContract, Ownable {
+
+    constructor() Ownable(msg.sender) {}
+
+    IERC1155 public token;
+    address public treasury;
+
+    error AlreadyInitialized();
+    error TransferFailed();
+    error ArraysLengthMismatch();
+    error NotEnoughtApprovedTokens();
+    error ContractNotInitialized();
+
+    bool private initialized;
+
+    modifier notInitialized() {
+        require(!initialized, AlreadyInitialized());
+        _;
+    }
+
+    function airdrop(address[] calldata receivers, uint256[] calldata amounts, uint256[] calldata tokenId) external onlyOwner {
+        if(!initialized) revert ContractNotInitialized();
+        if(receivers.length != amounts.length && receivers.length != tokenId.length) revert ArraysLengthMismatch();
+        if(!token.isApprovedForAll(treasury, address(this))) revert NotEnoughtApprovedTokens();
+        
+        for(uint256 i = 0; i < amounts.length; i++) {
+            token.safeTransferFrom(treasury, receivers[i], tokenId[i], amounts[i], "");
+        }
+    }
+
+    function initialize(bytes memory _initData) external notInitialized returns(bool){
+        (address _token, address _treasury, address _owner) = abi.decode(_initData, (address, address, address));
+
+        token = IERC1155(_token);
+        treasury = _treasury;
+
+        Ownable.transferOwnership(_owner);
+
+        initialized = true;
+
+        return true;
+    }
+
+    function getInitData(address _token, address _treasury, address _owner) external pure returns(bytes memory) {
+        return abi.encode(_token, _treasury, _owner);
+    }
+}
